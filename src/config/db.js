064529307a2f862bb946e7954b3a4fc8f5e18f14
AmriteshRane13 <amriteshrane46@gmail.com>
2026-02-27 19:15:25 +0530

    Changes to the db password

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const db = require("mysql2");
 
 const connection = db.createConnection({
   host: "localhost",
   user: "root",
-  password: "Amritesh@01",
-  database: "Express_trial",
+  password: process.env.DB_PASSWORD, // Update with your MySQL password
+  database: process.env.DB_NAME, // Update with your database name
 });
 
 connection.connect((err) => {
